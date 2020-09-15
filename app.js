739600b069b75ab996508db3e36e9a32a4e36ccc
Denzel <chiagozieani145@gmail.com>
2020-09-15 13:32:27 +0100

    Backend

--- app.js
+++ app.js
@@ -17,14 +17,12 @@ app.get('/', (req, res) => {
     res.redirect('/blogs')
 })
 cloudinary.config({
-    cloud_name: "dchsw3ztg",
-    api_key: "233568856113656",
-    api_secret: "Uh3sZPzhyFFTieVzZPly3co3q3s"
+
 });
 
 // console.log(process.env.API_KEY);
 
-mongoose.connect("mongodb+srv://Denzel:mechaD00dle@blog.zcoup.mongodb.net/blog?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(port, () => {
         console.log(`Server started on port ${port}`);
     })).catch((err) => console.log(err))
